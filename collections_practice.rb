965a38af458f9438f9a758b395112f6d01280b6c
Megan Collier <meganraec@live.com>
2018-03-20 01:50:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,17 +4,17 @@ end
 
 def sort_array_desc(array)
 end
-# 
+#
 # Build a method `sort_array_desc` that takes in an array of integers and returns a copy of the array with the integers in descending order. Remember that `.sort` takes a block in which you can specify how you want your array sorted.
 
 def sort_array_char_count
 end
-# 
+#
 # Build a method `sort_array_char_count` that takes in an array of strings and returns a copy of the array with the strings ordered in ascending order by length. Remember that `.sort` takes a block in which you can specify how you want your array sorted.
 
 def swap_elements(array)
 end
-# 
+#
 # Build a method `swap_elements` that takes in an array and swaps the second and third elements. Remember that array indices start at `0`, so the second element has an index of `1` and the third element has an index of `2`.
 # **Advanced:** *Try building a method* `swap_elements_from_to` *that takes in three arguments,* `array, index, destination_index`*, that will allow you to specify the index of the element you would like to move to a new index. So:*
 def swap_elements_from_to(array, index, destination_index)
@@ -26,30 +26,29 @@ end
 # **Advanced #2:** *Try writing test coverage for it!*
 
 def reverse_array(array)
-end 
+end
 # Build a method `reverse_array` that takes in an array of integers and returns a copy of the array with the elements in reverse order.
 
 def kesha_maker(array)
 end
-# 
+#
 # Build a method called `kesha_maker` that takes in an array of strings and replaces the third character in each string with a `$` ("dollar sign")—Ke$ha style. Use the `.each` method to iterate and build a new array to return at the end of your method, just like we did in the "My Each" lab.
 
 def find_a(array)
 end
-# 
+#
 # Build a method `find_a` that returns all the strings in the array passed to it that `start_with?` (**hint**) the letter `"a"`. You'll want to use a high level iterator for this that finds, selects, or detects elements based on a condition.
 
 def sum_array(array)
 # .inject
 end
 # Build a method `sum_array` that adds together all of the integers in the array and returns their sum.
-# 
+#
 # **Advanced:** *Try using the* `.inject` *method here.*
 
 def add_s(array)
 end
-# 
+#
 # Build a method that adds an `"s"` to each word in the array except for the second element in the array ("feet" is already plural).
-# 
+#
 # **Advanced:** *Iterators in Ruby are chainable, see if you can use* `.each_with_index` *in addition to* `.collect` *to solve this one in an elegant way. What happens if you write:*
-
