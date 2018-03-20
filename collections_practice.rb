207a81b2f24869da754f41eb5ab4548ffa866efc
Megan Collier <meganraec@live.com>
2018-03-20 02:13:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,6 +13,10 @@ end
 def swap_elements(array)
   array.sort {|x| array[0] <=> array[2]}
 end
+
+def swap_elements_from_to(array, index, destination_index)
+  array.sort {|x, y|}
+end
 #
 # Build a method `swap_elements` that takes in an array and swaps the second and third elements. Remember that array indices start at `0`, so the second element has an index of `1` and the third element has an index of `2`.
 # **Advanced:** *Try building a method* `swap_elements_from_to` *that takes in three arguments,* `array, index, destination_index`*, that will allow you to specify the index of the element you would like to move to a new index. So:*
